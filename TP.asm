2c4f4917682e244a535d5a0d9466f6ecd42119c1
EduFernandes97 <edu.cadete.fernandes@gmail.com>
2017-06-02 14:53:20 +0100

    SO FALTA ACABAR TOP

--- TP.asm
+++ TP.asm
@@ -20,9 +20,9 @@ dseg	segment para public 'data'
 	contaColuna		db		0
 	contaLinha		db		0
 	POSx			db		?
-	POSy			db		19
+	POSy			db		20
 	POSxa			db		?
-	POSya			db		19	
+	POSya			db		20	
 	Car				db		32	; Guarda um caracter do Ecran 
 	CarN				db		32	; Guarda um caracter do Ecran 
 	Cor				db		7	; Guarda os atributos de cor do caracter
@@ -58,12 +58,16 @@ dseg	segment para public 'data'
 					db		'000010000000000000000000000000000000000|'
 					db		'000010000000000000000000000000000000000|'
 					db		'000010000000000000000000000000000000000|'
+					db		'000010000000000000000000000000000000000|'
 					db		'0000I0000000000000000000000000000000000|'
 					db		'000000000000000000000000000000000000000|',0
 					
 					
 	bufferTop		db		'                                             ';12 bytes para tempo + 10 bytes para nome + 1 para fim
 	NomeUser		db		'          '
+	
+	
+	buffer         	db      22 dup (41 dup (' '));CRIAR MAZE
 					
 	
 	
@@ -90,16 +94,16 @@ APAGA_ECRAN	PROC
 		PUSH CX
 		PUSH SI
 		XOR	BX,BX
-		MOV	CX,100*100
-		mov bx,160
-		MOV SI,BX
+		MOV	CX,25*80
+		mov bx,0
+		; MOV SI,BX
 APAGA:	
 		MOV	AL,' '
 		MOV	BYTE PTR ES:[BX],AL
 		MOV	BYTE PTR ES:[BX+1],7
 		INC	BX
 		INC BX
-		INC SI
+		; INC SI
 		LOOP	APAGA
 		POP SI
 		POP CX
@@ -1393,16 +1397,16 @@ MazeFich:
 	
 	
 CriaMaze:
-	;call criaMaze;TODO
-	;call MostraMazeCriado;TODO
-	cmp flagFich, 0
-	je FimOpJogar
+	
+	call MostraMazeFich
+	; cmp flagFich, 0
+	; je FimOpJogar
 	jmp JOGAR
 	
 	
 JOGAR:
-	MOV POSy, 19
-	MOV POSya, 19
+	MOV POSy, 20
+	MOV POSya, 20
 	call Ler_TEMPO
 	call JOGO
 	call FimJogo
@@ -1411,6 +1415,318 @@ JOGAR:
 	
 FimOpJogar:	ret
 opJogar	endp
+
+
+criarMaze proc
+	; mov al, 01h
+	; int 21h
+	
+	mov POSx, 5
+	mov POSy, 5
+	
+
+	call	apaga_ecran
+		
+		mov dl, 219
+		mov ah, 02h
+		mov cx, 40
+		preencheCima:
+			goto_xy	cl,1
+			mov dl, 219
+			mov ah, 02h
+			int 21h
+			goto_xy	cl,19
+			mov dl, 219
+			mov ah, 02h
+			int 21h
+			goto_xy	cl,20
+			mov dl, 219
+			mov ah, 02h
+			int 21h
+		loop preencheCima
+		
+		mov cx, 20
+		preencheLados:
+			goto_xy	0,cl
+			mov dl, 219
+			mov ah, 02h
+			int 21h
+			goto_xy	40,cl
+			mov dl, 219
+			mov ah, 02h
+			int 21h
+		loop preencheLados
+		
+		goto_xy	20,1
+		mov ah, 02h
+		mov dl, 'X'
+		int 21h
+		
+		goto_xy	20,19
+		mov ah, 02h
+		mov dl, 'I'
+		int 21h
+		
+		;Obter a posição
+		; dec		POSy
+		; dec		POSx
+
+CICLO:	goto_xy	POSx,POSy
+IMPRIME:	mov		ah, 02h
+		mov		dl, Car
+		int		21H			
+		goto_xy	POSx,POSy
+		
+		call 		LE_TECLA
+		cmp		ah, 1
+		je		ESTEND
+		CMP 		AL, 27		; ESCAPE
+		JE		Guarda
+
+ZERO:	CMP 		AL, 48		; Tecla 0
+		JNE		UM
+		mov		Car, 32		;ESPAÇO
+		jmp		CICLO					
+		
+UM:		CMP 		AL, 49		; Tecla 1
+		JNE		ESTEND
+		mov		Car, 219		;Caracter CHEIO
+		jmp		CICLO		
+; I:	CMP 		AL, 49h
+	; JNE		I2
+	; CALL CriarInicio
+	; jmp CICLO
+	
+; I2:	CMP 		AL, 69h
+	; JNE		X
+	; CALL CriarInicio
+	; jmp CICLO
+; X:	CMP 		AL, 58h
+	; JNE		X2
+	; CALL CriarFim
+	; jmp CICLO
+; X2:	CMP 		AL, 78h
+	; JNE		CICLO
+	; CALL CriarFim
+	; jmp CICLO
+	
+ESTEND:	cmp 		al,48h
+		jne		BAIXO
+		cmp POSy, 3
+		jnge CICLO
+		dec		POSy		;cima
+		
+		jmp		CICLO
+
+BAIXO:	cmp		al,50h
+		jne		ESQUERDA
+		cmp POSy, 17
+		jnle CICLO
+		inc 		POSy		;Baixo
+		jmp		CICLO
+
+ESQUERDA:
+		cmp		al,4Bh
+		jne		DIREITA
+		cmp POSx, 2
+		jnge CICLO
+		dec		POSx		;Esquerda
+		jmp		CICLO
+
+DIREITA:
+		cmp		al,4Dh
+		jne		CICLO 
+		cmp POSx, 38
+		jnle CICLO
+		inc		POSx		;Direita
+		jmp		CICLO
+
+Guarda:	
+
+	xor si, si
+	xor bl, bl
+	xor cl, cl
+	inc cl
+	i:
+		cmp bl, 21
+		je sai
+		
+		xor cl, cl
+		j:
+			cmp cl, 41
+			je avancaI
+
+			goto_xy cl, bl
+			mov 	ah, 08h
+			mov		bh,0		; numero da página
+			int		10h			
+			
+			
+			cmp al, 219
+			je parede
+			
+			cmp al, ' '
+			je vazio
+			
+			cmp al, 'I'
+			je moveBuffer
+			
+			cmp al, 'X'
+			je moveBuffer
+			
+			parede:
+				mov al, 30h
+				jmp moveBuffer
+				
+			vazio:
+				mov al, 31h
+				jmp moveBuffer
+			
+			
+			
+			moveBuffer:
+			
+				mov buffer[si], al
+				inc si
+				inc cl
+		jmp j
+
+	avancaI:
+		inc bl
+		mov al, '|'
+		mov buffer[si], al
+		INC SI
+	
+		mov al, 10
+		mov buffer[si], al
+		INC SI
+		
+		jmp i
+
+
+	sai:
+	; mov		ah,4CH
+	; INT		21H
+	
+			; mov ah, 01h
+		; int	21h
+
+
+
+
+
+
+
+	; XOR	BX,BX
+	; XOR	si,si
+	; mov cx, 25*80
+	; for1:
+		; MOV	al, BYTE PTR ES:[BX]
+		; INC	BX
+		; INC BX
+		
+		; mov dl, al;auxiliar
+		; mov ax, si
+		; mov dh, bl
+		; mov bl, 41
+		; div bl
+		; mov bl, dh
+		; mov al, dl
+		; cmp ah, 0
+		; je novaLinha
+		
+		
+		
+		
+		
+		; cmp al, 219
+		; je parede
+		
+		; cmp al, ' '
+		; je vazio
+		
+		; cmp al, 'I'
+		; je moveBuffer
+		
+		; cmp al, 'X'
+		; je moveBuffer
+		
+		; parede:
+			; mov al, 31h
+			; jmp moveBuffer
+			
+		; vazio:
+			; mov al, 30h
+			; jmp moveBuffer
+		
+		; prox:
+			; inc si
+			; loop for1
+		
+		; novaLinha:
+			; dec bx
+			; dec bx
+			; add bx, 41
+			
+			mov al, '|'
+			mov buffer[si], al
+			INC SI
+		
+			mov al, 10
+			
+			
+		; moveBuffer:
+			; mov ax, dx
+			; mov buffer[si], al
+			; INC SI
+	; loop for1
+	
+
+
+	
+	mov	ah, 3ch			; abrir ficheiro para escrita 
+	mov	cx, 00H			; tipo de ficheiro
+	lea	dx, Fich			; dx contem endereco do nome do ficheiro 
+	int	21h				; abre efectivamente e AX vai ficar com o Handle do ficheiro 
+	jnc	escreve			; se não acontecer erro vai vamos escrever
+	
+	mov	ah, 09h			; Aconteceu erro na leitura
+	lea	dx, msgErrorCreate ;//mete endereco da msg em dx
+	int	21h ; //carater lido em AL
+	
+	jmp	fim
+
+escreve:
+	mov	bx, ax			; para escrever BX deve conter o Handle 
+	mov	ah, 40h			; indica que vamos escrever 
+    	
+	lea	dx, buffer			; Vamos escrever o que estiver no endereço DX
+	mov	cx, 902			; vamos escrever multiplos bytes duma vez só
+	int	21h				; faz a escrita 
+	jnc	close				; se não acontecer erro fecha o ficheiro 
+	
+	mov	ah, 09h
+	lea	dx, msgErrorWrite
+	int	21h
+close:
+	mov	ah,3eh			; indica que vamos fechar
+	int	21h				; fecha mesmo
+	jnc	fim				; se não acontecer erro termina
+	
+	mov	ah, 09h
+	lea	dx, msgErrorClose
+	int	21h
+	
+	
+fim:	
+	ret
+criarMaze endp
+
+
+
+
+
+
 
 
 
@@ -1482,6 +1798,7 @@ opMazeConfig	proc
 		jmp FIM
 	OP3:
 		mov auxConfigMaze, 3
+		call criarMaze
 		jmp FIM
 	
 	
